697dcf61787de357a8c0d973122313b046e6c0bc
Shuchi Jain <shuchijain@zipcoders-MacBook-Pro.local>
2019-02-23 12:32:35 -0500

    adding some hand changes

--- src/main/java/io/zipcoder/casino/utilities/CasinoGame/Hand.java
+++ src/main/java/io/zipcoder/casino/utilities/CasinoGame/Hand.java
@@ -1,20 +1,64 @@
 package io.zipcoder.casino.utilities.CasinoGame;
+import java.util.ArrayList;
 
 public class Hand {
-    private java.util.ArrayList<Object> hand;
 
-    public int getValue() {
+
+
+   ArrayList<Card> hand = new ArrayList<Card>();
+
+    public Hand(ArrayList<Card> hand) {
+        this.hand = hand;
+    }
+
+    public int getBlackJackValue(ArrayList hand)
+
+    {
+
+       // Card card = new Card(B);
+
+
+        return 0;
+
+
+
+    }
+
+
+    public int getGeneralValue() {
+
+
         return 0;
     }
 
-    public int getSize() {
-        return 0;
-    }
+
+        public int getSize(ArrayList<Card> hand)
+
+
+    {
+
+        return hand.size();
+
+
+
+
+        }
+
+
 
     public void clear() {
+
+        {
+            hand.clear();
+        }
+
+
     }
 
     public String toString() {
+
         return null;
+
+
     }
 }
