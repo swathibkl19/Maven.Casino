2be7f80389bec8d120cb3bf605a1fab4c4c9b42c
Michael Krohn <michaelkrohn@zipcoders-MacBook-Pro-96.local>
2019-02-23 11:39:53 -0500

    added CardInfo class

--- src/main/java/io/zipcoder/casino/utilities/CasinoGame/Dice.java
+++ src/main/java/io/zipcoder/casino/utilities/CasinoGame/Dice.java
@@ -1,9 +1,14 @@
 package io.zipcoder.casino.utilities.CasinoGame;
 
+import java.util.Random;
+
 public class Dice {
     public java.util.Random random;
+    private Random diceVal = new Random();
+
 
     public Dice(Integer numberOfDice, Integer diceSize) {
+
     }
 
     public Integer getValue() {
