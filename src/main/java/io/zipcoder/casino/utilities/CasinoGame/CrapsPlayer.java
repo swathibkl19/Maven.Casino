0623f9c3cd446cb1e909e2ff21a70a98521fc240
Michael Krohn <michaelkrohn@zipcoders-MacBook-Pro-96.local>
2019-02-24 10:58:13 -0500

    Dice class finished and initial changed to CrapsPlayer

--- src/main/java/io/zipcoder/casino/utilities/CasinoGame/CrapsPlayer.java
+++ src/main/java/io/zipcoder/casino/utilities/CasinoGame/CrapsPlayer.java
@@ -1,11 +1,20 @@
 package io.zipcoder.casino.utilities.CasinoGame;
 
 public class CrapsPlayer implements GamblingPlayer {
+
+    String name;
+    Integer balance;
+    public CrapsPlayer(Player player) {
+        this.name = player.getName();
+        this.balance = player.getBalance();
+    }
+
     public Integer placeBet() {
+        
         return null;
     }
 
-    public void recieveWinnings() {
-
+    public void recieveWinnings(Integer winnings) {
+        balance += winnings;
     }
 }
