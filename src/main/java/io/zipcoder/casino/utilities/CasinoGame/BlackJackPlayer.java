6259a541dcc02071952e5fce51e3132b0d6d83fb
Swathi Bikumalia <swathibikumalia@zipcoders-MacBook-Pro.local>
2019-02-24 12:00:29 -0500

    blackjack player and tested

--- src/main/java/io/zipcoder/casino/utilities/CasinoGame/BlackJackPlayer.java
+++ src/main/java/io/zipcoder/casino/utilities/CasinoGame/BlackJackPlayer.java
@@ -1,27 +1,37 @@
 package io.zipcoder.casino.utilities.CasinoGame;
-import java.lang.*;
-
+//import java.io.Console;
+import java.io.InputStream;
+import java.io.PrintStream;
 public class BlackJackPlayer implements GamblingPlayer {
 
+    Console console = new Console();
     String name;
     Integer balance;
 
- public BlackJackPlayer(Player player) {
+ public BlackJackPlayer(Player player){
 
-     }
+    name = player.getName();
 
-    public Integer placeBet() {
-
-    //     Integer betValue ,balance;
-
-  // Player player = new Player("","");
-
-   //balance = player.balance - betValue;
-
-    //return  balance;
-    }
-
-    public void recieveWinnings() {
-
-    }
+    balance = player.getBalance();
 }
+
+    public void placeBet(Integer betValue) {
+
+        Integer remainingbalance = balance- betValue;
+
+
+    }
+
+    public Integer recieveWinnings() {
+
+        Integer integerInput = console.getIntegerInput("");
+
+            return integerInput;
+        }
+
+
+}
+
+
+
+
